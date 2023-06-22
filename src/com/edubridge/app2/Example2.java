5afc68a9c7ff80f06e6a7b3c0e78da4329765356
Remidi Hari Priya <Remidi Hari Priya@DESKTOP-3UQ3FH4>
2023-06-22 10:17:45 +0530

    Second program of Example

---
+++ src/com/edubridge/app2/Example2.java
@@ -0,0 +1,11 @@
+package com.edubridge.app2;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		int marks = 90;
+		System.out.println("Marks of student are "+ marks);
+
+	}
+
+}
