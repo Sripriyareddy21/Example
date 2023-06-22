e60d9ad30a49a41b02f1643de0526c64378e42c1
Remidi Hari Priya <Remidi Hari Priya@DESKTOP-3UQ3FH4>
2023-06-22 10:20:32 +0530

    Third Commit of Example project

---
+++ src/com/edubridge/app3/Example3.java
@@ -0,0 +1,11 @@
+package com.edubridge.app3;
+
+public class Example3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String dept = "ECE";
+      System.out.println("Department of Student is "+ dept);
+	}
+
+}
