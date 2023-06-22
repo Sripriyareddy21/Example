eddc0ff750cb9fb3f044c2db52ea3d4dedccdb2c
Remidi Hari Priya <Remidi Hari Priya@DESKTOP-3UQ3FH4>
2023-06-22 10:10:45 +0530

    Second Commit

---
+++ src/com/edubridge/app2/Example1.java
@@ -0,0 +1,11 @@
+package com.edubridge.app2;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		String name = "Sri";
+		System.out.println("Name of the person is "+ name);
+
+	}
+
+}
